2b3db3fab76bc5534ae778074d445ffef2afd7d0
Yash Karthik <52369876+YashKarthik@users.noreply.github.com>
2023-01-09 17:53:04 +0530

    CORS for API endpoint.

--- src/pages/api/read-user/[fid].ts
+++ src/pages/api/read-user/[fid].ts
@@ -4,8 +4,26 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { appRouter } from "../../../server/api/root";
 import { createTRPCContext } from "../../../server/api/trpc";
 import { z, ZodError } from 'zod';
+import Cors from 'cors'
+
+const cors = Cors({
+  methods: "GET"
+});
+
+const corsMiddleware = (req: NextApiRequest, res: NextApiResponse, fn: Function) => {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+      return resolve(result)
+    });
+  });
+}
 
 const fetchByFID = async (req: NextApiRequest, res: NextApiResponse) => {
+  await corsMiddleware(req, res, cors);
+
   const { fid } = req.query;
   const CheckFIDFormat = z.coerce.number();
 
